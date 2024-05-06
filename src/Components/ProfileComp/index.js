b5231a8cb300831bc056e198b9b611b999072ef7
thoparamvishwateja <thoparamvishwateja0000@gmail.com>
2024-05-06 21:02:15 +0530

    final commit

--- src/Components/ProfileComp/index.js
+++ src/Components/ProfileComp/index.js
@@ -33,25 +33,22 @@ const MorphingBackground = () => {
   }, []);
 
   return (
-    <div className="morphing-demo" >
+    <div className="morphing-demo">
       <svg ref={svgRef} viewBox="0 0 140 140">
-        <mask id="image-mask" x="0" y="0" width="100%" height="100%" >
+        <mask id="image-mask" x="0" y="0" width="100%" height="100%">
           <polygon ref={maskRef} points="70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369" fill="white" />
         </mask>
 
         <image
-  ref={imageRef}
-  // href="https://res.cloudinary.com/dcneemn9t/image/upload/v1714822094/image_profile_ranuc7.jpg"
-  href="art_image.png"
-  // href="https://res.cloudinary.com/dcneemn9t/image/upload/v1713427819/nxxtm9h62gale2psq94x.jpg"
-  x="28" y="32.5" // Adjusted x and y position to center the image
-  width="75%" height="75%"
-  preserveAspectRatio="xMidYMid slice"
-  mask="url(#image-mask)"
-/>
+          ref={imageRef}
+          href="https://res.cloudinary.com/dplcbpxda/image/upload/v1714986698/image_pro_1_atot1x.png"
+          x="28" y="32.5" 
+          width="75%" height="75%"
+          preserveAspectRatio="xMidYMid slice"
+          mask="url(#image-mask)"
+        />
 
-
-        <polygon className="polymorph" points="70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369" fill="none" stroke="pink"  />
+        <polygon className="polymorph" points="70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369" fill="none" stroke="pink" />
       </svg>
     </div>
   );
